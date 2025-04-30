c720e226e79c6cdfec24bd123910778ba4288794
Oscar Otero <oom@oscarotero.com>
2025-04-30 18:22:22 +0200

    auth

--- _cms.ts
+++ _cms.ts
@@ -1,3 +1,12 @@
 import cms from "top/_cms.ts";
 
+cms.git();
+
+const user = Deno.env.get("CMS_USER") ?? "admin";
+const pass = Deno.env.get("CMS_PASSWORD") ?? "";
+
+cms.auth({
+  [user]: pass,
+})
+
 export default cms;
